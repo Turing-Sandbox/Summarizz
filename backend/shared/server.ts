08e4873bf01297da2121519838adb5cdf420fb84
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2024-11-22 14:04:13 -0500

    fix comment route

--- backend/shared/server.ts
+++ backend/shared/server.ts
@@ -12,7 +12,7 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', commentRoutes);
+app.use('/comment', commentRoutes);
 app.use("/user", userRoutes);
 app.use("/content", contentRoutes);
 
