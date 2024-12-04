12e7904ce06a4a62d7ebb19e8c8f8f272f3d2822
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-12-04 00:59:48 -0500

    add: summarize logo, types.d.ts, organize content

---
+++ frontend/types.d.ts
@@ -0,0 +1,22 @@
+declare module '*.svg' {
+    const content: Icon;
+    export default content;
+}
+
+declare module '*.png' {
+    const content: Icon;
+    export default content;
+}
+
+declare module '*.jpg' {
+    const content: Icon;
+    export default content;
+}
+
+declare module "*.jpeg" {
+    const content: Icon;
+    export default content;
+}
+
+// ANCHOR - This is going to play a big role in the refactoring future!
+// ANCHOR - Summarizz 2025
