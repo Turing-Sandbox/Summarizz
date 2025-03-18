b71268094208055e31fcb5ff6bb2f697ec4ca8ca
Benjamin Lefebvre <blefebv1@my.centennialcollege.ca>
2025-03-18 15:08:39 -0400

    Forgot to remove imports

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,5 @@
 "use client";
 
-import Background from "@/components/Background";
-import AuthProvider from "../hooks/AuthProvider";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
 export default function Page() {
   return (
     <div className='main-content'>
