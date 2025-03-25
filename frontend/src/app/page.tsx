18419fc427c7c4f584476c7f90d225b380e0684f
Benjamin Lefebvre <blefebv1@my.centennialcollege.ca>
2025-03-25 16:46:54 -0400

    Move the Tile content in component. Implement Feed frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,171 @@
 "use client";
 
+import { useAuth } from "@/hooks/AuthProvider";
+import { Content } from "@/models/Content";
+import { useEffect, useState } from "react";
+import { apiURL } from "./scripts/api";
+import axios from "axios";
+import { HeartIcon } from "@heroicons/react/24/outline";
+import ContentTile from "@/components/content/ContentTile";
+import { User } from "@/models/User";
+
 export default function Page() {
+  const [trendingContent, setTrendingContent] = useState<Content[]>([]);
+  const [latestContent, setLatestContent] = useState<Content[]>([]);
+  const [personalizedContent, setPersonalizedContent] = useState<Content[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { userUID } = useAuth();
+  const [user, setUser] = useState<User | null>(null);
+
+  const [errorTrending, setErrorTrending] = useState<string | null>(null);
+  const [errorLatest, setErrorLatest] = useState<string | null>(null);
+  const [errorPersonalized, setErrorPersonalized] = useState<string | null>(
+    null
+  );
+
+  useEffect(() => {
+    const fetchContent = async () => {
+      setIsLoading(true);
+
+      setErrorTrending(null);
+      setErrorLatest(null);
+      setErrorPersonalized(null);
+
+      let userFetched = await fetchUser();
+      let latestFetched = await fetchLatestContent();
+      let trendingFetched = await fetchTrendingContent();
+      let personalizedFetched = await fetchPersonalizedContent();
+
+      if (!latestFetched) {
+        setErrorLatest(
+          "Failed to fetch latest content. Please reload the page or contact support."
+        );
+      }
+
+      if (!trendingFetched) {
+        setErrorTrending(
+          "Failed to fetch trending content. Please reload the page or contact support."
+        );
+      }
+
+      if (!personalizedFetched) {
+        setErrorPersonalized(
+          "Failed to fetch personalized content. Please reload the page or contact support."
+        );
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchContent();
+  }, [userUID]);
+
+  async function fetchUser(): Promise<boolean> {
+    try {
+      const userResponse = await axios.get(`${apiURL}/user/${userUID}`, {
+        timeout: 5000,
+      });
+
+      if (userResponse.status === 200 || userResponse.status === 201) {
+        setUser(userResponse.data);
+        return true;
+      } else {
+        setUser(null);
+      }
+    } catch (userError) {
+      setUser(null);
+    }
+
+    return false;
+  }
+
+  async function fetchTrendingContent(): Promise<boolean> {
+    try {
+      const trendingResponse = await axios.get(
+        `${apiURL}/content/feed/trending`,
+        { timeout: 5000 }
+      );
+
+      if (trendingResponse.data && trendingResponse.data.success) {
+        setTrendingContent(trendingResponse.data.trendingContent);
+        return true;
+      } else {
+        setTrendingContent([]);
+      }
+    } catch (trendingError) {
+      setTrendingContent([]);
+    }
+
+    return false;
+  }
+
+  async function fetchLatestContent(): Promise<boolean> {
+    try {
+      const contentResponse = await axios.get(`${apiURL}/content`, {
+        timeout: 5000,
+      });
+
+      if (contentResponse.data && contentResponse.data.success) {
+        setLatestContent(contentResponse.data.content);
+        return true;
+      } else {
+        setLatestContent([]);
+      }
+    } catch (contentError) {
+      setLatestContent([]);
+    }
+
+    return false;
+  }
+
+  async function fetchPersonalizedContent(): Promise<boolean> {
+    try {
+      const personalizedResponse = await axios.get(
+        `${apiURL}/content/feed/personalized`,
+        { timeout: 5000 }
+      );
+
+      if (personalizedResponse.data && personalizedResponse.data.success) {
+        setPersonalizedContent(personalizedResponse.data.personalizedContent);
+        return true;
+      } else {
+        setPersonalizedContent([]);
+      }
+    } catch (personalizedError) {
+      setPersonalizedContent([]);
+    }
+    return false;
+  }
+
   return (
     <div className='main-content'>
-      <div className='header'>
-        <h1>Summarizz</h1>
-        <p>Summarize your articles, videos, and more with ease.</p>
+      {isLoading && <p>Loading...</p>}
+
+      {user && <h1>Welcome, {user?.firstName}</h1>}
+      <h2>Top Trending</h2>
+      <div className='content-list'>
+        {trendingContent.map((content, index) => (
+          <ContentTile key={content.id} content={content} index={index} />
+        ))}
       </div>
+      {errorTrending && <p className='error'>{errorTrending}</p>}
+
+      <h2>Latest Post</h2>
+      <div className='content-list'>
+        {latestContent.map((content, index) => (
+          <ContentTile key={content.id} content={content} index={index} />
+        ))}
+      </div>
+      {errorLatest && <p className='error'>{errorLatest}</p>}
+
+      <h2>For You</h2>
+      <div className='content-list'>
+        {personalizedContent.map((content, index) => (
+          <ContentTile key={content.id} content={content} index={index} />
+        ))}
+      </div>
+      {errorPersonalized && <p className='error'>{errorPersonalized}</p>}
     </div>
   );
 }
---
+++ frontend/src/components/content/ContentTile.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { Content } from "@/models/Content";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import {
+  BookmarkIcon as BookmarkIconOutline,
+  EyeIcon as EyeIconOutline,
+  HeartIcon as HeartIconOutline,
+  ShareIcon as ShareIconOutline,
+  TrashIcon as TrashIconOutline,
+} from "@heroicons/react/24/outline";
+import {
+  HeartIcon as HeartIconFilled,
+  ShareIcon as ShareIconFilled,
+  BookmarkIcon as BookmarkIconFilled,
+} from "@heroicons/react/24/solid";
+
+import "@/app/styles/content/contentTile.scss";
+import { useAuth } from "@/hooks/AuthProvider";
+
+interface ContentTileProps {
+  content: Content;
+  index: number;
+  deleteShareOption?: boolean;
+  handleUnshare?: (contentId: string) => void;
+}
+
+export default function ContentTile({
+  content,
+  index,
+  deleteShareOption = false,
+  handleUnshare = () => {},
+}: ContentTileProps) {
+  const router = useRouter();
+  const { userUID } = useAuth();
+
+  return (
+    <div
+      key={content.id || index}
+      className='content-list-item'
+      onClick={() => router.push(`/content/${content.id}`)}
+    >
+      <div className='content-tile-header'>
+        <h3 className='content-item-title'>{content.title}</h3>
+        {/* Unshare Button */}
+        {deleteShareOption && (
+          <button
+            className='icon-button unshare'
+            onClick={(e) => {
+              e.stopPropagation();
+              handleUnshare(content.id);
+            }}
+            title='Unshare Content'
+          >
+            <TrashIconOutline className='icon delete' />
+          </button>
+        )}
+      </div>
+
+      <p>
+        {content.dateCreated
+          ? `${content.dateCreated.toLocaleString("en-US", {
+              month: "short",
+            })} ${content.dateCreated.getDate()}${
+              content.readtime ? ` - ${content.readtime} min read` : ""
+            }`
+          : ""}
+      </p>
+      {content.thumbnail && (
+        <div className='content-thumbnail-container'>
+          <Image
+            src={content.thumbnail}
+            alt='Thumbnail'
+            width={200}
+            height={200}
+            className='content-thumbnail'
+          />
+        </div>
+      )}
+
+      <div className='content-stats'>
+        <span className='stat-item'>
+          {userUID && content.peopleWhoLiked?.includes(userUID) ? (
+            <HeartIconFilled className='stat-icon' />
+          ) : (
+            <HeartIconOutline className='stat-icon' />
+          )}
+          <p className='stat-number'>{content.likes || 0}</p>
+        </span>
+        <span className='stat-item'>
+          <EyeIconOutline className='stat-icon' />
+          <p className='stat-number'>{content.views || 0}</p>
+        </span>
+        <span className='stat-item'>
+          {userUID && content.sharedBy?.includes(userUID) ? (
+            <ShareIconFilled className='stat-icon' />
+          ) : (
+            <ShareIconOutline className='stat-icon' />
+          )}
+          <p className='stat-number'>{content.shares || 0}</p>
+        </span>
+        <span className='stat-item'>
+          {userUID && content.bookmarkedBy?.includes(userUID) ? (
+            <BookmarkIconFilled className='stat-icon' />
+          ) : (
+            <BookmarkIconOutline className='stat-icon' />
+          )}
+          <p className='stat-number'>{content.bookmarkedBy?.length || 0}</p>
+        </span>
+      </div>
+    </div>
+  );
+}
