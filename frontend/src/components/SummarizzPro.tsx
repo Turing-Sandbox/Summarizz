7bf9b31ce30f4f40c001762094d2ce63282c4c78
Benjamin Lefebvre <blefebv1@my.centennialcollege.ca>
2025-03-30 22:21:30 -0400

    Improved UI for Summarizz Pro

---
+++ frontend/src/components/SummarizzPro.tsx
@@ -0,0 +1,10 @@
+import "@/app/styles/summarizzPro.scss";
+
+export default function SummarizzPro() {
+  return (
+    <div className='summarizz-pro-container'>
+      <h1 className='summarizz-logo summarizz-logo-pro'>Summarizz</h1>
+      <h2 className='summarizz-pro-tag'>PRO</h2>
+    </div>
+  );
+}
