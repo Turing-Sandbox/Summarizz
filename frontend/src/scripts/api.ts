750e8ac4f417df5a97d71d68bd69ce5881bdddce
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2025-08-28 14:19:06 -0400

    fix url API

--- frontend/src/scripts/api.ts
+++ frontend/src/scripts/api.ts
@@ -1,4 +1,4 @@
-let baseApiUrl = "comp313-402-team3-w25-production.up.railway.app";
+let baseApiUrl = "https://comp313-402-team3-w25-production.up.railway.app";
 // let baseApiUrl = "https://ts-backend.up.railway.app";
 
 if (process.env.NODE_ENV === "development") {
